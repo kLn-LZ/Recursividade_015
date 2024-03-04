0028dc7750644c93a3468cc47a2eae985e1b3f9a
kLn-LZ <160866564+kLn-LZ@users.noreply.github.com>
2024-03-04 17:10:39 -0300

    adicionando comentários

--- src/Controller/Fibonnaci.java
+++ src/Controller/Fibonnaci.java
@@ -7,11 +7,14 @@ public class Fibonnaci {
 
     public int funcFibonacci (int n ) {
 
+
+        // Redundância de posição ( 1º = 1, 2º = 1), ou seja, o limite é a segunda posição
         if (n < 2) {
 
             return n;
         }
 
+        // Regra da sequência, o número da iteração menos 1 somando com a próxima iteração menos 2
         return funcFibonacci(n-1) + funcFibonacci(n -2);
 
     }
